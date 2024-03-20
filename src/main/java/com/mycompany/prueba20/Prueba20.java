be81d7ec7e7b72c4cca471a9705c6411241298b4
sirmvm <matias.valladares@gmail.com>
2024-03-20 12:58:05 -0300

    hola android1

---
+++ src/main/java/com/mycompany/prueba20/Prueba20.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba20;
+
+/**
+ *
+ * @author matia
+ */
+public class Prueba20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
